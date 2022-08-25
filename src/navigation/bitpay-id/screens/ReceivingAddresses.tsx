648bb44d1bb5f27f5a34001e92744df03dc76624
Marty Alcala <marty@bitpay.com>
2022-08-25 16:54:43 -0400

    add action buttons

--- src/navigation/bitpay-id/screens/ReceivingAddresses.tsx
+++ src/navigation/bitpay-id/screens/ReceivingAddresses.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/native';
 import {View} from 'react-native';
 import {useTheme} from '@react-navigation/native';
-import {Br} from '../../../components/styled/Containers';
+import {Br, HEIGHT} from '../../../components/styled/Containers';
 import {CurrencyListIcons} from '../../../constants/SupportedCurrencyOptions';
 import {H3, H5, Paragraph} from '../../../components/styled/Text';
 import {BaseText} from '../../wallet/components/KeyDropdownOption';
@@ -11,6 +11,8 @@ import ChevronRightSvg from '../../../../assets/img/angle-right.svg';
 import AddSvg from '../../../../assets/img/add.svg';
 import AddWhiteSvg from '../../../../assets/img/add-white.svg';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import Button from '../../../components/button/Button';
+import {t} from 'i18next';
 
 const AddressItem = styled.View`
   align-items: center;
@@ -42,51 +44,62 @@ const AddButton = styled.View`
 const ReceivingAddresses = () => {
   const theme = useTheme();
   return (
-    <View style={{padding: 16}}>
-      <H3>Choose your BitPay ID Receiving Addresses</H3>
-      <Br />
-      <Paragraph>
-        Decide what wallets you would like to receive to when a friend sends
-        crypto to your email address. Each incoming payment will be sent to a
-        newly generated address.
-      </Paragraph>
-      <Br />
-      <Br />
-      <H5>Receiving Addresses</H5>
-      <TouchableOpacity activeOpacity={0.8}>
-        <AddressItem>
-          {CurrencyListIcons.btc({height: 25})}
-          <AddressItemText>
-            Select a <BaseText style={{fontSize: 16}}>BTC Wallet</BaseText>
-          </AddressItemText>
-          <ChevronRightSvg height={16} />
-        </AddressItem>
-      </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.8}>
-        <AddressItem>
-          {CurrencyListIcons.bch({height: 25})}
-          <AddressItemText>
-            Select a <BaseText style={{fontSize: 16}}>BCH Wallet</BaseText>
-          </AddressItemText>
-          <ChevronRightSvg height={16} />
-        </AddressItem>
-      </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.8}>
-        <AddressItem>
-          {CurrencyListIcons.eth({height: 25})}
-          <AddressItemText>
-            Select a <BaseText style={{fontSize: 16}}>ETH Wallet</BaseText>
-          </AddressItemText>
-          <ChevronRightSvg height={16} />
-        </AddressItem>
-      </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.8}>
-        <AddressItem>
-          <AddButton>{theme.dark ? <AddWhiteSvg /> : <AddSvg />}</AddButton>
-          <AddressItemText>Add Wallet</AddressItemText>
-          <ChevronRightSvg height={16} />
-        </AddressItem>
-      </TouchableOpacity>
+    <View style={{padding: 16, flexDirection: 'column', height: HEIGHT - 110}}>
+      <View style={{flexGrow: 1}}>
+        <H3>Choose your BitPay ID Receiving Addresses</H3>
+        <Br />
+        <Paragraph>
+          Decide what wallets you would like to receive to when a friend sends
+          crypto to your email address. Each incoming payment will be sent to a
+          newly generated address.
+        </Paragraph>
+        <Br />
+        <Br />
+        <H5>Receiving Addresses</H5>
+        <TouchableOpacity activeOpacity={0.8}>
+          <AddressItem>
+            {CurrencyListIcons.btc({height: 25})}
+            <AddressItemText>
+              Select a <BaseText style={{fontSize: 16}}>BTC Wallet</BaseText>
+            </AddressItemText>
+            <ChevronRightSvg height={16} />
+          </AddressItem>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.8}>
+          <AddressItem>
+            {CurrencyListIcons.bch({height: 25})}
+            <AddressItemText>
+              Select a <BaseText style={{fontSize: 16}}>BCH Wallet</BaseText>
+            </AddressItemText>
+            <ChevronRightSvg height={16} />
+          </AddressItem>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.8}>
+          <AddressItem>
+            {CurrencyListIcons.eth({height: 25})}
+            <AddressItemText>
+              Select a <BaseText style={{fontSize: 16}}>ETH Wallet</BaseText>
+            </AddressItemText>
+            <ChevronRightSvg height={16} />
+          </AddressItem>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.8}>
+          <AddressItem>
+            <AddButton>{theme.dark ? <AddWhiteSvg /> : <AddSvg />}</AddButton>
+            <AddressItemText>Add Wallet</AddressItemText>
+            <ChevronRightSvg height={16} />
+          </AddressItem>
+        </TouchableOpacity>
+      </View>
+      <Button buttonStyle={'primary'} onPress={() => console.log('save')}>
+        {t('Save Defaults')}
+      </Button>
+      <Button
+        buttonStyle={'primary'}
+        buttonType={'link'}
+        onPress={() => console.log('save')}>
+        {t('Add Custom Address')}
+      </Button>
     </View>
   );
 };
