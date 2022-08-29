8cfd2158806ec22f9e399d4591458ce65d8ff178
Marty Alcala <marty@bitpay.com>
2022-08-29 19:32:54 -0400

    init email container

--- src/navigation/bitpay-id/screens/RecevingEnabled.tsx
+++ src/navigation/bitpay-id/screens/RecevingEnabled.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import styled from 'styled-components/native';
 import Button from '../../../components/button/Button';
 import {Br, HEIGHT} from '../../../components/styled/Containers';
-import {H3, Paragraph, TextAlign} from '../../../components/styled/Text';
+import SuccessSvg from '../../../../assets/img/success.svg';
+import {
+  BaseText,
+  H3,
+  Paragraph,
+  TextAlign,
+} from '../../../components/styled/Text';
 import {t} from 'i18next';
 
 const ViewContainer = styled.View`
@@ -18,10 +24,28 @@ const ViewBody = styled.View`
   padding: 20px;
 `;
 
+const EmailContainer = styled.View`
+  background-color: rgba(34, 64, 196, 0.35);
+  align-items: center;
+  height: 48px;
+  padding: 0 14px 0 17px;
+  border-radius: 48px;
+  width: 100%;
+  max-width: 300px;
+  margin-top: 32px;
+  flex-direction: row;
+`;
+
+const EmailText = styled(BaseText)`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
 const ReceivingEnabled = () => {
   return (
     <ViewContainer>
       <ViewBody>
+        <SuccessSvg height={50} width={50} style={{marginBottom: 24}} />
         <TextAlign align="center">
           <H3>Receiving to BitPay ID has been enabled!</H3>
         </TextAlign>
@@ -31,7 +55,10 @@ const ReceivingEnabled = () => {
             Your friends and family can now send you crypto to your BitPay ID.
           </Paragraph>
         </TextAlign>
-        <Br />
+        <EmailContainer>
+          <EmailText style={{flexGrow: 1}}>satoshi@nakamoto.com</EmailText>
+          <SuccessSvg height={20} width={20} />
+        </EmailContainer>
       </ViewBody>
       <Button buttonStyle={'primary'} onPress={() => console.log('hi')}>
         {t('Go back to settings')}
