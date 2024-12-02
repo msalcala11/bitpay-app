431bbab1ebd19bdf800918d37bc279ff239267eb
Marty Alcala <marty@bitpay.com>
2024-12-02 17:57:35 -0500

    cleanup

--- android/app/src/main/java/com/bitpay/wallet/MainApplication.kt
+++ android/app/src/main/java/com/bitpay/wallet/MainApplication.kt
@@ -48,7 +48,6 @@ class MainApplication : Application(), ReactApplication {
     override fun onCreate() {
         super.onCreate()
         val context: Context = this
-        // SoLoader.init(this, false)
         SoLoader.init(this, OpenSourceMergedSoMapping)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             load()
