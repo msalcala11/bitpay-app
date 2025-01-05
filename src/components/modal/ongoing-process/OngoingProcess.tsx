36ea6d42bf27ad9b68f38c12b1b6062473e50c78
Marty Alcala <marty@bitpay.com>
2025-01-04 23:52:13 -0500

    migrate ongoing process to bottom-sheet-modal

--- src/components/modal/ongoing-process/OngoingProcess.tsx
+++ src/components/modal/ongoing-process/OngoingProcess.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
 import {ActivityIndicator} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import styled from 'styled-components/native';
+import {
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+  BottomSheetModal,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
 import {LightBlack, SlateDark, White} from '../../../styles/colors';
 import {useAppSelector} from '../../../utils/hooks';
 import {BlurContainer} from '../../blur/Blur';
 import {BaseText} from '../../styled/Text';
+import {HEIGHT} from '../../../components/styled/Containers';
+
 import BaseModal from '../base/BaseModal';
 
 export type OnGoingProcessMessages =
@@ -75,7 +88,77 @@ const OnGoingProcessModal: React.FC = () => {
   const isVisible = useAppSelector(({APP}) => APP.showOnGoingProcessModal);
   const appWasInit = useAppSelector(({APP}) => APP.appWasInit);
 
-  return (
+  const modalLibrary: 'bottom-sheet' | 'modal' = 'bottom-sheet';
+
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  const opacityFadeDuration = 200;
+  const opacity = useSharedValue(0);
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
+  useEffect(() => {
+    if (isVisible && appWasInit) {
+      bottomSheetModalRef.current?.present();
+      setTimeout(() => {
+        opacity.value = withTiming(1, {duration: opacityFadeDuration});
+      }, 300);
+    } else {
+      opacity.value = withTiming(0, {duration: opacityFadeDuration});
+      setTimeout(() => {
+        bottomSheetModalRef.current?.dismiss();
+      }, opacityFadeDuration - 50);
+    }
+  }, [appWasInit, isVisible, opacity]);
+
+  const renderBackdrop = useCallback(
+    (props: BottomSheetBackdropProps) => (
+      <BottomSheetBackdrop
+        {...props}
+        opacity={0.4}
+        pressBehavior={'none'}
+        disappearsOnIndex={-1}
+        appearsOnIndex={0}
+      />
+    ),
+    [],
+  );
+
+  return modalLibrary === 'bottom-sheet' ? (
+    <BottomSheetModal
+      detached={true}
+      bottomInset={HEIGHT / 2}
+      backdropComponent={renderBackdrop}
+      backgroundStyle={{borderRadius: 18}}
+      enableDismissOnClose={true}
+      enableDynamicSizing={true}
+      enableOverDrag={false}
+      enablePanDownToClose={false}
+      handleComponent={null}
+      backgroundComponent={null}
+      index={0}
+      ref={bottomSheetModalRef}>
+      <BottomSheetView
+        style={{
+          alignItems: 'center',
+        }}>
+        <Animated.View style={[animatedStyles]}>
+          <OnGoingProcessContainer>
+            <Row>
+              <ActivityIndicatorContainer>
+                <ActivityIndicator color={SlateDark} />
+              </ActivityIndicatorContainer>
+              <Message>{message}</Message>
+              <BlurContainer />
+            </Row>
+          </OnGoingProcessContainer>
+        </Animated.View>
+      </BottomSheetView>
+    </BottomSheetModal>
+  ) : (
     <BaseModal
       id={'ongoingProcess'}
       isVisible={appWasInit && isVisible}
