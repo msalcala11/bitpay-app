4972c465a1977158469db6d4c8e5b2d017c1a661
Marty Alcala <marty@bitpay.com>
2025-12-16 16:31:10 -0500

    fix

--- src/navigation/tabs/shop/components/Bills.tsx
+++ src/navigation/tabs/shop/components/Bills.tsx
@@ -219,37 +219,57 @@ export const Bills = () => {
     <SectionContainer
       style={{minHeight: HEIGHT - (Platform.OS === 'android' ? 200 : 225)}}>
       {!isVerified ? (
-        <>
-          <BillPitch />
-          <Button
-            height={50}
-            onPress={() => {
-              dispatch(
-                AppEffects.openUrlWithInAppBrowser(
-                  `${verificationBaseUrl}&context=createAccount`,
-                ),
-              );
-              dispatch(Analytics.track('Bill Pay - Clicked Sign Up'));
-            }}>
-            {t('Sign Up')}
-          </Button>
-          <View style={{height: 10}} />
-          <Button
-            height={50}
-            buttonStyle="secondary"
-            onPress={() => {
-              dispatch(
-                AppEffects.openUrlWithInAppBrowser(
-                  `${verificationBaseUrl}&context=login`,
-                ),
-              );
-              dispatch(
-                Analytics.track('Bill Pay - Clicked I Already Have an Account'),
-              );
-            }}>
-            {t('I already have an account')}
-          </Button>
-        </>
+        billPayServicePaused ? (
+          <>
+            <BillPitch />
+            {isJoinedWaitlist ? (
+              <Paragraph style={{textAlign: 'center', fontSize: 14}}>
+                {t('You have joined the waitlist.')}
+              </Paragraph>
+            ) : (
+              <Button
+                state={waitlistButtonState}
+                style={{width: WIDTH - 32, marginTop: 24}}
+                height={50}
+                buttonStyle="secondary"
+                onPress={onSubmit}>
+                {t('Join waitlist')}
+              </Button>
+            )}
+          </>
+        ) : (
+          <>
+            <BillPitch />
+            <Button
+              height={50}
+              onPress={() => {
+                dispatch(
+                  AppEffects.openUrlWithInAppBrowser(
+                    `${verificationBaseUrl}&context=createAccount`,
+                  ),
+                );
+                dispatch(Analytics.track('Bill Pay - Clicked Sign Up'));
+              }}>
+              {t('Sign Up')}
+            </Button>
+            <View style={{height: 10}} />
+            <Button
+              height={50}
+              buttonStyle="secondary"
+              onPress={() => {
+                dispatch(
+                  AppEffects.openUrlWithInAppBrowser(
+                    `${verificationBaseUrl}&context=login`,
+                  ),
+                );
+                dispatch(
+                  Analytics.track('Bill Pay - Clicked I Already Have an Account'),
+                );
+              }}>
+              {t('I already have an account')}
+            </Button>
+          </>
+        )
       ) : (
         <>
           {billPayServicePaused ? (
