2008deed653d0c49d27b9fc9c00351633c0c9c2f
Marty Alcala <marty@bitpay.com>
2026-01-27 17:02:03 -0500

    ref: replace address with crypto balance in exchange rate wallet item

--- src/navigation/wallet/screens/ExchangeRate.tsx
+++ src/navigation/wallet/screens/ExchangeRate.tsx
@@ -69,7 +69,6 @@ import type {Key} from '../../../store/wallet/wallet.models';
 import type {RootState} from '../../../store';
 import {
   calculatePercentageDifference,
-  formatCryptoAddress,
   formatFiatAmount,
   getRateByCurrencyName,
   sleep,
@@ -1752,8 +1751,10 @@ const ExchangeRate = () => {
                     {ui.walletName}
                   </WalletName>
                   <WalletSub numberOfLines={1} ellipsizeMode="tail">
-                    {ui.receiveAddress
-                      ? formatCryptoAddress(ui.receiveAddress)
+                    {hideAllBalances
+                      ? '****'
+                      : ui.cryptoBalance
+                      ? `${ui.cryptoBalance} ${ui.currencyAbbreviation}`
                       : ''}
                   </WalletSub>
                 </WalletLeft>
