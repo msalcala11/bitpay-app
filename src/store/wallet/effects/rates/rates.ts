92c93dc5cf057ae7cf1f0f5408eb0f50a04ec505
Marty Alcala <marty@bitpay.com>
2026-02-12 22:39:50 -0500

    Revert "restore prod endpoint"
    This reverts commit 199ceb2511ef840ba863e9f5d1b67804214ae25e.

--- src/store/wallet/effects/rates/rates.ts
+++ src/store/wallet/effects/rates/rates.ts
@@ -47,8 +47,8 @@ import type {Key, Wallet} from '../../wallet.models';
 import {normalizeFiatRateSeriesCoin} from '../../../../utils/portfolio/core/pnl/rates';
 import {isSortedByTsAsc} from '../../../../utils/portfolio/timeSeries';
 
-const FIAT_RATE_SERIES_BASE_URL = `${BASE_BWS_URL}/v4/fiatrates`;
-// const FIAT_RATE_SERIES_BASE_URL = `http://localhost:3232/bws/api/v4/fiatrates`;
+// const FIAT_RATE_SERIES_BASE_URL = `${BASE_BWS_URL}/v4/fiatrates`;
+const FIAT_RATE_SERIES_BASE_URL = `http://localhost:3232/bws/api/v4/fiatrates`;
 
 
 const FIAT_RATE_SERIES_INTERVAL_DAYS: Record<
