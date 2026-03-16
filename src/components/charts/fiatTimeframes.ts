1a62a048963fe14f2e730f9c1574ce8cb8f224f4
Marty Alcala <marty@bitpay.com>
2026-03-15 20:38:45 -0400

    translate all chart labels

--- src/components/charts/fiatTimeframes.ts
+++ src/components/charts/fiatTimeframes.ts
@@ -13,10 +13,7 @@ export const getFiatChartTimeframeOptions = (
 ): Array<{label: string; value: FiatRateInterval}> => {
   return FIAT_CHART_TIMEFRAME_VALUES.map(value => ({
     value,
-    label:
-      value === 'ALL'
-        ? t(getFiatTimeframeMetadata(value).displayLabel)
-        : getFiatTimeframeMetadata(value).displayLabel,
+    label: t(getFiatTimeframeMetadata(value).displayLabel)
   }));
 };
 
